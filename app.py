bd9b7c4a585d1c6d0a1d4188d0bc2af10448d9ee
Shuiui <shubhipathak055@gmail.com>
2025-09-07 18:48:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ headache = st.number_input("Headache", min_value=0, max_value=1)
 nausea = st.number_input("Nausea", min_value=0, max_value=1)
 vomiting = st.number_input("Vomiting", min_value=0, max_value=1)
 fatigue = st.number_input("Fatigue", min_value=0, max_value=1)
-joint_pain = st.number_input("Joint Pain", min_value=0.0, max_value=1,)
+joint_pain = st.number_input("Joint Pain", min_value=0, max_value=1,)
 skin_rash = st.number_input("Skin Rash", min_value=0, max_value=1,)
 cough = st.number_input("Cough", min_value=0, max_value=1)
 weight_loss = st.number_input("wieght loss", min_value=0, max_value=1)
@@ -23,5 +23,6 @@ if st.button("Predict"):
     prediction = model.predict(features)[0]
     result = "⚠️ Disease Detected" if prediction == 1 else "✅ No Disease Detected"
     st.subheader(f"Result: {result}")
+
 
 
